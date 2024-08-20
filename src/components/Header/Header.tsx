4a68c5245bc34bf5fbd512dffc743c9d92210f13
Emil307 <emilnovikov392@gmail.com>
2024-08-20 16:06:50 +0600

    [Feat] change header styles after scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./styles.module.css";
 
 export const Header = () => {
-  return <div>Header</div>;
+  const [isMounted, setIsMounted] = useState<boolean>(false);
+  const [isIsland, setIsIsland] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!isMounted) {
+      setIsMounted(true);
+      return;
+    }
+
+    const callback = () => {
+      if (window.pageYOffset > 0) {
+        setIsIsland(true);
+        return;
+      }
+      setIsIsland(false);
+      return;
+    };
+
+    window.addEventListener("scroll", callback);
+
+    return () => window.removeEventListener("scroll", callback);
+  }, [isMounted]);
+
+  return (
+    <header className={`${styles.section} ${isIsland && styles.island}`}>
+      <h2>Mojo.</h2>
+      <div className={styles.links}>
+        <a href="#">Services</a>
+        <a href="#">Work</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+      <div className={styles.button}>
+        <button className={styles.start}>Start a project</button>
+      </div>
+    </header>
+  );
 };
